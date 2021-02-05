94459bbd83d4e3ce15c4da1d1af4d0fec6544894
Sanjay <sanjayar2200@gmail.com>
2021-02-05 14:42:42 +0530

    adding

---
+++ Day_16/test.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  3 12:31:48 2021
+
+@author: sanja
+"""
+
+import streamlit as st
+
+st.title('Model Deployment: Logistic regression')
+
+st.write("Implementing the logistic regression")
+
